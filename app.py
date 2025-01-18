b42e895edc959dfe1b4c4a551d3709e3f533b92b
Rakesh_Seerla <seerlarakesh916022@gmail.com>
2025-01-18 23:35:26 +0530

    app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import os
 import streamlit as st
 import pickle
 
-# Correct paths to model and vectorizer files
-model_path = "C:/Users/seerl/OneDrive/Desktop/NLP_Project/NLP_PROJECT_MODEL.pkl"
-vectorizer_path = "C:/Users/seerl/OneDrive/Desktop/NLP_Project/tfidf_vectorization.pkl"
+# Correct paths to model and vectorizer files using relative paths
+model_path = "NLP_PROJECT_MODEL.pkl"
+vectorizer_path = "tfidf_vectorization.pkl"
 
 # Ensure the paths are correct
 assert os.path.exists(model_path), "Model file not found"
